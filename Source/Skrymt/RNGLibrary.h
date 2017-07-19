2f0f0e0be8636de2e3f9887d346f5238353fcb85
Henrik Melsom <melsom.henrik@gmail.com>
2017-07-19 19:09:57 +0200

    fixed RNGLibrary compile error and added coments

--- Source/Skrymt/RNGLibrary.h
+++ Source/Skrymt/RNGLibrary.h
@@ -15,25 +15,39 @@ class SKRYMT_API URNGLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	// Generates a random integer
+	/*****************************
+	* Standard Random Generation *
+	*****************************/
+
+	// Generates a random integer between min (inclusive) and max (inclusive)
 	UFUNCTION(BlueprintCallable, Category = "RNG")
 	static int32 RandomInt(int32 Min, int32 Max);
 	static int32 RandomInt(int32 Max);
 	static int32 RandomInt();
-	static int32 RandomInt(int32 Seed, int32 Min, int32 Max);
-	static int32 RandomInt(int32 Seed, int32 Max);
-	static int32 RandomInt(int32 Seed);
 
-	// Generates a random float
+	// Generates a random float between min (inclusive) and max (inclusive)
 	UFUNCTION(BlueprintCallable, Category = "RNG")
 	static float RandomFloat(float Min, float Max);
 	static float RandomFloat(float Max);
 	static float RandomFloat();
-	static float RandomFloat(int32 Seed, float Min, float Max);
-	static float RandomFloat(int32 Seed, float Max);
-	static float RandomFloat(int32 Seed);
 
 	// Generates a random float
 	UFUNCTION(BlueprintCallable, Category = "RNG")
 	static float SimplexNoise(float x, float y, float z, float w);
+
+	/***************************
+	* Seeded Random Generation *
+	***************************/
+
+	// Generates a random seeded integer between min (inclusive) and max (inclusive)
+	UFUNCTION(BlueprintCallable, Category = "RNG")
+	static int32 RandomSeededInt(int32 Seed, int32 Min, int32 Max);
+	static int32 RandomSeededInt(int32 Seed, int32 Max);
+	static int32 RandomSeededInt(int32 Seed);
+
+	// Generates a random seeded float between min (inclusive) and max (inclusive)
+	UFUNCTION(BlueprintCallable, Category = "RNG")
+	static float RandomSeededFloat(int32 Seed, float Min, float Max);
+	static float RandomSeededFloat(int32 Seed, float Max);
+	static float RandomSeededFloat(int32 Seed);
 };
