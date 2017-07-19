10f7c5bb1220fdabaab6bf3536cd0094b76391fe
Unstable605 <hangra-4@student.ltu.se>
2017-07-19 09:26:14 +0200

    Merge branch 'master' of https://github.com/ArcticBeardStudio/Skrymt

---
+++ Source/Skrymt/TextureLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TextureLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKRYMT_API UTextureLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// Gets the color value of a textures pixel
+	UFUNCTION(BlueprintCallable, Category = "Texture")
+	FColor GetTexturePixel(UTexture2D Texture, int32 X, int32 Y);
+};
