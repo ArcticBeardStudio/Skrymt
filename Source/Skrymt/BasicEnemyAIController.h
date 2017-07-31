f77b71b1793d5e895095347a125c2b5d2ba0ee4f
Unstable605 <hangra-4@student.ltu.se>
2017-07-31 10:38:46 +0200

    messages and assets
    moved assets into folders and added combatmessage interface

--- Source/Skrymt/BasicEnemyAIController.h
+++ Source/Skrymt/BasicEnemyAIController.h
@@ -41,41 +41,23 @@ private:
 
 protected:
 	/** A Sight Sense config for our AI */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Perception")
-	//UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere)
 		UAISenseConfig_Sight* Sight;
 
 	/** The Behavior Tree that contains the logic of our AI */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ai")
-	//UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere)
 		UBehaviorTree* BehaviorTree;
 
 	/** The Perception Component of our AI */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Perception")
+	UPROPERTY(EditAnywhere)
 		UAIPerceptionComponent* AIPerceptionComponent;
 
 public:
 
-	//ABasicEnemyAIController();
+	
 
-	ABasicEnemyAIController(const FObjectInitializer& ObjectInitializer);
-	//{
-	//	Sight = ObjectInitializer.CreateDefaultSubobject<UAISenseConfig_Sight>(this, TEXT("SightSense"));
-
-
-	//	Sight->SightRadius = 2000.f;
-	//	Sight->LoseSightRadius = 2100.f;
-	//	Sight->PeripheralVisionAngleDegrees = 360.f;
-	//	//Sight->SetMaxAge(15.f);
-
-	//	//Tell the sight sense to detect everything
-	//	Sight->DetectionByAffiliation.bDetectEnemies = true;
-	//	Sight->DetectionByAffiliation.bDetectFriendlies = true;
-	//	Sight->DetectionByAffiliation.bDetectNeutrals = true;
-
-	//	//Register the sight sense to our Perception Component
-	//	AIPerceptionComponent->ConfigureSense(*Sight);
-	//}
+	ABasicEnemyAIController();
+	
 
 	virtual void Possess(APawn* InPawn) override;
 
